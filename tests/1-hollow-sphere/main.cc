3fb51934d0f722f81177e37a3e51f9d30cf2eb61
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2020-01-03 17:21:56 +0200

    Thermal stress calculation in a hollow sphere, part 5

--- tests/1-hollow-sphere/main.cc
+++ tests/1-hollow-sphere/main.cc
@@ -32,22 +32,29 @@ private:
   void
   output_results() const;
 
+  double r_a, r_b;
+  double T_a, T_b;
+
   Point<dim>                   center;
   const SphericalManifold<dim> manifold;
 
   Triangulation<dim> triangulation;
-  FE_Q<dim>          fe;
-  DoFHandler<dim>    dof_handler;
+  FE_Q<dim>          fe_temp;
+  DoFHandler<dim>    dh_temp;
 
   Vector<double> temperature;
 };
 
 template <int dim>
 Problem<dim>::Problem(unsigned int order)
-  : center(Point<dim>())
+  : r_a(0.5)
+  , r_b(1.0)
+  , T_a(500)
+  , T_b{1000}
+  , center(Point<dim>())
   , manifold(SphericalManifold<dim>(center))
-  , fe(order)
-  , dof_handler(triangulation)
+  , fe_temp(order)
+  , dh_temp(triangulation)
 {}
 
 template <int dim>
@@ -63,7 +70,9 @@ template <int dim>
 void
 Problem<dim>::make_grid()
 {
-  GridGenerator::hyper_shell(triangulation, center, 0.5, 1.0, 0, true);
+  Assert(r_a > 0, ExcMessage("r_a must be positive"));
+  Assert(r_b > r_a, ExcMessage("r_b must be larger than r_b"));
+  GridGenerator::hyper_shell(triangulation, center, r_a, r_b, 0, true);
 
   triangulation.set_all_manifold_ids(0);
   triangulation.set_manifold(0, manifold);
@@ -79,20 +88,23 @@ template <int dim>
 void
 Problem<dim>::initialize()
 {
-  dof_handler.distribute_dofs(fe);
-  const unsigned int n_dofs = dof_handler.n_dofs();
+  dh_temp.distribute_dofs(fe_temp);
+  const unsigned int n_dofs = dh_temp.n_dofs();
   std::cout << "Number of degrees of freedom: " << n_dofs << "\n";
 
   temperature.reinit(n_dofs);
 
   std::vector<Point<dim>> support_points(n_dofs);
   DoFTools::map_dofs_to_support_points(MappingQ1<dim>(),
-                                       dof_handler,
+                                       dh_temp,
                                        support_points);
 
+  // Set the temperature using analytical solution in spherical coordinates.
+  const double c1 = r_a * (T_a - T_b) / (1 - r_a / r_b);
+  const double c2 = (T_b - T_a * r_a / r_b) / (1 - r_a / r_b);
   for (unsigned int i = 0; i < n_dofs; ++i)
     {
-      temperature[i] = support_points[i].norm();
+      temperature[i] = c1 / support_points[i].norm() + c2;
     }
 }
 
@@ -102,7 +114,7 @@ Problem<dim>::output_results() const
 {
   DataOut<dim> data_out;
 
-  data_out.attach_dof_handler(dof_handler);
+  data_out.attach_dof_handler(dh_temp);
   data_out.add_data_vector(temperature, "T");
 
   data_out.build_patches();
