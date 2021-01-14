1d8ae2ff8092d759ea62089b753f83d4bb142b5d
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2021-01-14 12:46:06 +0200

    Implement temperature-dependent electrical conductivity
    Store parameters in problem.prm

--- applications/Inductive-heating/main.cc
+++ applications/Inductive-heating/main.cc
@@ -24,15 +24,66 @@ private:
   void
   initialize();
 
+  void
+  apply_q_em(void);
+
+  void
+  calculate_temperature(void);
+
   TemperatureSolver<dim> temperature_solver;
   DislocationSolver<dim> dislocation_solver;
+
+  // normalized Joulean heat flux density
+  Vector<double> q0;
+
+  constexpr static unsigned int boundary_id = 0;
+
+  ParameterHandler prm;
+
+  FunctionParser<1> electrical_conductivity;
 };
 
 template <int dim>
 Problem<dim>::Problem(unsigned int order)
   : temperature_solver(order)
   , dislocation_solver(order)
-{}
+{
+  prm.declare_entry("Initial temperature",
+                    "1000",
+                    Patterns::Double(0),
+                    "Initial temperature in K");
+
+  prm.declare_entry("Inductor current",
+                    "100",
+                    Patterns::Double(0),
+                    "Effective inductor current in A");
+
+  prm.declare_entry("Reference electrical conductivity",
+                    "5e4",
+                    Patterns::Double(0),
+                    "Reference electrical conductivity (qEM.vtu) in S/m");
+
+  prm.declare_entry("Electrical conductivity",
+                    "5e4",
+                    Patterns::Anything(),
+                    "Electrical conductivity in S/m");
+
+  try
+    {
+      prm.parse_input("problem.prm");
+    }
+  catch (std::exception &e)
+    {
+      std::cout << e.what() << "\n";
+
+      std::ofstream of("problem-default.prm");
+      prm.print_parameters(of, ParameterHandler::Text);
+    }
+
+  electrical_conductivity.initialize("T",
+                                     prm.get("Electrical conductivity"),
+                                     typename FunctionParser<1>::ConstMap());
+}
 
 template <int dim>
 void
@@ -41,21 +92,36 @@ Problem<dim>::run()
   make_grid();
   initialize();
 
-  while (true)
-    {
-      const bool keep_going = temperature_solver.solve();
-
-      if (!keep_going)
-        break;
-    };
-
-  temperature_solver.output_results();
+  calculate_temperature();
 
   // initialize dislocations and stresses
   dislocation_solver.initialize();
   dislocation_solver.get_temperature() = temperature_solver.get_temperature();
   dislocation_solver.solve(true);
   dislocation_solver.output_results();
+}
+
+template <int dim>
+void
+Problem<dim>::calculate_temperature(void)
+{
+  double max_dT;
+
+  do
+    {
+      Vector<double> temperature = temperature_solver.get_temperature();
+
+      apply_q_em();
+      temperature_solver.solve();
+
+      temperature -= temperature_solver.get_temperature();
+      max_dT = temperature.linfty_norm();
+
+      std::cout << "max_dT=" << max_dT << " K\n";
+    }
+  while (max_dT > 1);
+
+  temperature_solver.output_results();
 }
 
 template <int dim>
@@ -79,13 +145,11 @@ Problem<dim>::initialize()
   temperature_solver.initialize(); // sets T=0
 
   Vector<double> &temperature = temperature_solver.get_temperature();
-  temperature.add(1000);
+  temperature.add(prm.get_double("Initial temperature"));
 
   std::vector<Point<dim>> points;
   std::vector<bool>       boundary_dofs;
-  unsigned int            boundary_id = 0;
   temperature_solver.get_boundary_points(boundary_id, points, boundary_dofs);
-  Vector<double> q(points.size());
 
   SurfaceInterpolator3D surf;
   surf.read_vtu("qEM.vtu");
@@ -94,10 +158,27 @@ Problem<dim>::initialize()
                SurfaceInterpolator3D::PointField,
                "q");
   surf.interpolate(
-    SurfaceInterpolator3D::PointField, "q", points, boundary_dofs, q);
+    SurfaceInterpolator3D::PointField, "q", points, boundary_dofs, q0);
 
-  // scale by the square of current
-  q *= 1e-6 * sqr(105);
+  // normalize for future use
+  q0 *= 1e-6 * std::sqrt(prm.get_double("Reference electrical conductivity"));
+}
+
+template <int dim>
+void
+Problem<dim>::apply_q_em(void)
+{
+  const Vector<double> &temperature = temperature_solver.get_temperature();
+
+  Vector<double> q = q0;
+
+  // apply the current and temperature-dependent electrical conductivity
+  const double i2 = sqr(prm.get_double("Inductor current"));
+  for (unsigned int i = 0; i < q.size(); ++i)
+    {
+      const double s = electrical_conductivity.value(Point<1>(temperature[i]));
+      q[i] *= i2 / std::sqrt(s);
+    }
 
   // Physical parameters from https://doi.org/10.1016/j.jcrysgro.2020.125842
   std::function<double(double)> emissivity       = [](double) { return 0.57; };
