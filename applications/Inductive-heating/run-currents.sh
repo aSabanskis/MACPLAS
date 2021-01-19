37312312840158c80d91ed12b4c1fe66e9c3a17a
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2021-01-19 13:00:21 +0200

    Add script for calculation of steady-state temperature field and thermal stresses

---
+++ applications/Inductive-heating/run-currents.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Inductor currents, A
+arr_I=(30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 130 135 140)
+
+for i in "${!arr_I[@]}"; do
+    I=${arr_I[$i]}
+    r="I$I"
+
+    if [[ -d "$r" ]]; then
+        echo "$r" exists, skipping
+        continue
+    fi
+
+    echo "$r"
+    mkdir -p "$r"
+
+    sed -i "s/set Inductor current.*/set Inductor current = $I/g" problem.prm
+    cp -- *.prm "$r"
+
+    ./macplas-inductive-heating > "$r/log"
+
+    mv -- *-3d* "$r"
+done
