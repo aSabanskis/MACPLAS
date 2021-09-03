998917dfda5906760fb4425b8011d237cf6ec84a
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2021-09-03 19:08:44 +0300

    Crystal growth example: add temperature solver

--- applications/Crystal-growth/main.cc
+++ applications/Crystal-growth/main.cc
@@ -5,6 +5,8 @@
 
 #include <fstream>
 
+#include "../../include/temperature_solver.h"
+
 using namespace dealii;
 
 template <int dim>
@@ -20,18 +22,34 @@ private:
   void
   make_grid();
 
+  void
+  initialize_temperature();
+
+  TemperatureSolver<dim> temperature_solver;
+
   Triangulation<dim> triangulation;
+
+  ParameterHandler prm;
 };
 
 template <int dim>
 Problem<dim>::Problem(const unsigned int order, const bool use_default_prm)
-{}
+  : temperature_solver(order, use_default_prm)
+{
+  prm.declare_entry("Initial temperature",
+                    "1000",
+                    Patterns::Double(0),
+                    "Initial temperature T_0 in K");
+}
 
 template <int dim>
 void
 Problem<dim>::run()
 {
   make_grid();
+  initialize_temperature();
+
+  temperature_solver.output_vtk();
 }
 
 template <int dim>
@@ -43,6 +61,8 @@ Problem<dim>::make_grid()
   std::ifstream f("mesh-" + std::to_string(dim) + "d.msh");
   gi.read_msh(f);
 
+  temperature_solver.get_mesh().copy_triangulation(triangulation);
+
   // print info
   std::cout << "Number of cells: " << triangulation.n_cells() << "\n"
             << "Number of points: " << triangulation.n_vertices() << "\n";
@@ -50,6 +70,19 @@ Problem<dim>::make_grid()
   const auto bounds = triangulation.get_boundary_ids();
   for (const auto &b : bounds)
     std::cout << "Boundary no. " << (int)b << "\n";
+}
+
+template <int dim>
+void
+Problem<dim>::initialize_temperature()
+{
+  temperature_solver.initialize(); // sets T=0
+
+  Vector<double> &temperature = temperature_solver.get_temperature();
+  temperature.add(prm.get_double("Initial temperature"));
+
+  temperature_solver.output_mesh();
+  temperature_solver.output_parameter_table();
 }
 
 int
