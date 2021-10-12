7f3ea0606bc2e328e2bea2116f7ce07c634cc07e
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2021-10-12 14:12:27 +0300

    Crystal growth example: parametric setup script, part 3

--- applications/Crystal-growth/parametric-setup.py
+++ applications/Crystal-growth/parametric-setup.py
@@ -26,6 +26,7 @@ params = {
     "t2": 120,
     "Ta1": 300,
     "Ta2": 940,
+    "mr": 85,
     "rho": 5370,
 }
 
@@ -49,8 +50,8 @@ plt.savefig("crystal-shape.png", dpi=150, bbox_inches="tight")
 # convert to SI
 x = x * 1e-3
 y = y * 1e-3
-L_max = x[-1]
-print(f"L_max={L_max*1e3:g} mm")
+L_total = x[-1]
+print(f"L_total={L_total*1e3:g} mm")
 np.savetxt("crystal-shape.dat", np.c_[x, y], fmt="%g")
 R = interpolate.interp1d(x, y)
 
@@ -80,7 +81,28 @@ V = interpolate.interp1d(x, y)
 # MASS
 m_total = 0
 rho = params.get("rho")
-L_arr, dL = np.linspace(0, L_max, int(L_max / 1e-4), retstep=True)
+L_arr, dL = np.linspace(0, L_total, int(L_total / 1e-4), retstep=True)
 for L in L_arr:
     m_total += rho * dL * np.pi * R(L) ** 2
-print(f"m={m_total*1e3:g} g")
+print(f"m_total={m_total*1e3:g} g")
+
+# MAX TIME
+m_melt = m_total
+m_residual = params.get("mr") * 1e-3
+L = 0
+t = 0
+dt = 2.0
+while True:
+    t += dt
+    dL = V(t) * dt
+    L += dL
+    dm = rho * dL * np.pi * R(L) ** 2
+    m_melt -= dm
+    if L >= L_total or m_melt <= m_residual:
+        break
+t_max = t
+L_max = L
+m_crystal = m_total - m_melt
+print(f"t_max={t_max:g} s")
+print(f"L_max={L_max*1e3:g} mm")
+print(f"m_crystal={m_crystal*1e3:g} g")
