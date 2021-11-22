927313fd03cf03e30f39303e34500a1ac74c7dd1
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2021-11-22 19:14:18 +0200

    Crystal growth example: set parameters, part 8

--- applications/Crystal-growth/run-transient-temperature-dislocation.sh
+++ applications/Crystal-growth/run-transient-temperature-dislocation.sh
@@ -22,13 +22,22 @@ cp parameters/* .
 
 setup_parameters
 
+sed -Ei "s|(set Fix interface fields *= *).*|\1 true|" problem.prm
+sed -Ei "s|(set Interpolation method *= *).*|\1 advection|" problem.prm
+sed -Ei "s|(set Stabilization multiplier *= *).*|\1 0|" advection.prm
+sed -Ei "s|(set Stabilization type *= *).*|\1 SUPG|" advection.prm
+sed -Ei "s|(set Linear solver type *= *).*|\1 fgmres|" advection.prm
+sed -Ei "s|(set Linear solver iterations *= *).*|\1 100|" advection.prm
+sed -Ei "s|(set Time stepping theta *= *).*|\1 1|" advection.prm
+sed -Ei "s|(set Courant number *= *).*|\1 0|" advection.prm
+
 sed -Ei "s|(set Temperature only *= *).*|\1 false|" problem.prm
 sed -Ei "s|(set Time step *= *).*|\1 $dt|" dislocation.prm
 sed -Ei "s|(set Time step *= *).*|\1 $dt|" temperature.prm
 sed -Ei "s|(set Max time step *= *).*|\1 $dt|" dislocation.prm
-sed -Ei "s|(set Min time step *= *).*|\1 0.01|" dislocation.prm
+sed -Ei "s|(set Min time step *= *).*|\1 $dt|" dislocation.prm
 
-sed -Ei "s|(set Initial dislocation density *= *).*|\1 1e0|" dislocation.prm
+sed -Ei "s|(set Initial dislocation density *= *).*|\1 1e4|" dislocation.prm
 sed -Ei "s|(set Critical stress *= *).*|\1 0|" dislocation.prm
 
 sed -Ei "s|(set Max dstrain_c *= *).*|\1 1e-6|" dislocation.prm
