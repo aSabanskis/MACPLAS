80174e92c2cb3be311d99578796941402059f8a5
Andrejs Sabanskis <andrejs.sabanskis@lu.lv>
2024-08-28 15:48:38 +0300

    Bending example, part 10

--- applications/Bending-test/run-T-sweep.sh
+++ applications/Bending-test/run-T-sweep.sh
@@ -2,11 +2,14 @@
 
 source ./helper.sh
 
-# dim, order, threads
+# order, threads
 initialize # 2 0
 
 # Temperature
 arr_T=(400 500 600 700 800 900)
+
+# Pressure
+p=2e5
 
 clean_results
 setup_parameters
@@ -15,7 +18,7 @@ setup_parameters
 
 for i in "${!arr_T[@]}"; do
     T=${arr_T[$i]}
-    id="T$T"
+    id="p$p-T$T"
     r="results-$id"
     echo "$id"
 
@@ -27,11 +30,15 @@ for i in "${!arr_T[@]}"; do
     sed -Ei "s|(set Initial temperature *= *).*|\1$T|"   problem.prm
     sed -Ei "s|(set Reference temperature *= *).*|\1$T|" stress.prm
 
+    sed -Ei "s|(set Pressure *= *).*|\1$p|" problem.prm
+
     mkdir -p $r
 
     ./macplas-bending order "$order" >"$r/log"
-    mv probes-dislocation-3d.txt "$r/"
-    mv result-dislocation-3d-order$order-*.vtk "$r/"
+
+    ./plot-probes-minmax.gnu
+
+    mv probes-dislocation-3d.txt probes-minmax.pdf result-dislocation-3d-order$order-*.vtk "$r/"
 done
 
 ./plot-probes-compare.gnu
